2cb4d9d864ff088c49dff47ee327e89ee5fb78b3
moonstoper <sjkachhap@outlook.com>
2021-06-10 12:10:31 +0530

    fn name change + some test cases

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-function displayTest() {
-var str1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var str2="abcdefghijklmnopqrstuvwxyz";
+const str = {
+    alphaC : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    alphaS:"abcdefghijklmnopqrstuvwxyz",
 // console.log(displayTest(v))
-var str3="1234567890";
-var str4="!@#$%&";
+    numb:"1234567890",
+    special:"!@#$%&",
+}
+function passgenJs() {
+
 var value='';
 for(i=1;i<=2;i++){
 var char1=Math.floor(Math.random()
-* str1.length);
- value += str1.charAt(char1)
+    * str.alphaC.length);
+    // console.log(char1)
+ value += str.alphaC.charAt(char1)
  var char2=Math.floor(Math.random()
-* str2.length);
- value += str2.charAt(char2)
+* str.alphaS.length);
+ value += str.alphaS.charAt(char2)
  var char3=Math.floor(Math.random()
-* str3.length);
- value += str3.charAt(char3)
+* str.numb.length);
+ value += str.numb.charAt(char3)
  var char4=Math.floor(Math.random()
-* str4.length);
- value += str4.charAt(char4)
+* str.special.length);
+ value += str.special.charAt(char4)
  }
  return(value);
 }
- console.log(displayTest());
+ console.log(passgenJs());
 
- module.exports = displayTest
+ module.exports = passgenJs
