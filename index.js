ca4463e0cc2c9ece1bacef97ef6b9ef01ca4577b
moonstoper <sjkachhap@outlook.com>
2021-06-13 12:34:33 +0530

    added  error handling and removed readline.. check npm test

--- index.js
+++ index.js
@@ -1,69 +1,64 @@
 const str = {
-    alphaC : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    alphaS:"abcdefghijklmnopqrstuvwxyz",
-// console.log(displayTest(v))
-    numb:"1234567890",
-    special:"!@#$%&",
+  alphaC: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  alphaS: "abcdefghijklmnopqrstuvwxyz",
+  // console.log(displayTest(v))
+  numb: "1234567890",
+  special: "!@#$%&",
+};
+function RandomInt(n) {
+  return Math.floor(Math.random() * n);
 }
-function RandomInt(n){
-    return Math.floor(Math.random()*n);
-  }
 // Increasing randomness in password
-function shuffelPassword(s){
-      var arr=s.split('');
-      var n=arr.length;
+function shuffelPassword(s) {
+  var arr = s.split("");
+  var n = arr.length;
 
-      for(var i=0;i<n-1;i++){
-        var j=RandomInt(n);
+  for (var i = 0; i < n - 1; i++) {
+    var j = RandomInt(n);
 
-        var temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-      }
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
 
-      s=arr.join('');
-      return s;
+  s = arr.join("");
+  return s;
+}
+
+function passgenJs(length) {
+  try {
+    /* length property of passgenJs   @type number */
+    this.len = length || 8;
+    if (this.len < 8) {
+      throw new Error("Error : Lenght must be >= 8");
     }
+    if (typeof this.len === "string") {
+      throw new Error("Error : Length must be of type number");
+    }
+    var value = "";
+    if (this.len % 4 === 0) {
+      // in each iteration we are accessing random element from all strings
+      for (i = 1; i <= this.len / 4; i++) {
+        var char1 = Math.floor(Math.random() * str.alphaC.length);
+        // console.log(char1)
+        value += str.alphaC.charAt(char1);
+        var char2 = Math.floor(Math.random() * str.alphaS.length);
+        value += str.alphaS.charAt(char2);
+        var char3 = Math.floor(Math.random() * str.numb.length);
+        value += str.numb.charAt(char3);
+        var char4 = Math.floor(Math.random() * str.special.length);
+        value += str.special.charAt(char4);
+      }
+      var pass = shuffelPassword(value);
+      return pass;
+    } else {
+      throw new Error(
+        "Error : Not valid input. Enter in multiple of 4. min::8"
+      );
+    }
+  } catch (error) {
+    return error.message;
+  }
+}
 
-function passgenJs(len) {
-var value='';
-if(len%4==0)
-{
-// in each iteration we are accessing random element from all strings
-for(i=1;i<=len/4;i++){
-var char1=Math.floor(Math.random()
-    * str.alphaC.length);
-    // console.log(char1)
- value += str.alphaC.charAt(char1)
- var char2=Math.floor(Math.random()
-* str.alphaS.length);
- value += str.alphaS.charAt(char2)
- var char3=Math.floor(Math.random()
-* str.numb.length);
- value += str.numb.charAt(char3)
- var char4=Math.floor(Math.random()
-* str.special.length);
- value += str.special.charAt(char4)
- }
-   var pass=shuffelPassword(value);
-   return pass;
-}
-else{
-  console.log("Not valid input. Enter in multiple of 4.");
-}
-}
-// console.log(passgenJs());
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-// taking input from the user for the length of the password
-readline.question('Enter the Password length (multiple of 4 & greater than or equal to 8): ', length => {
-   var length=(`${length}`);
-  var pswd= passgenJs(length);
-  if(length%4==0)
-  console.log(pswd);
-  readline.close();
-});
-
- module.exports= passgenJs;
+module.exports = passgenJs;
--- index.test.js
+++ index.test.js
@@ -17,13 +17,27 @@ function numcountDefault(pass) {
     return len
 }
 
-test("default: Length of string: 8", () => {
-  expect(passgenJs().length).toBe(8);
-});
+describe("PASSWORD GENERATION", () => {
+  test("default : Length of string : 8", () => {
+    expect(passgenJs().length).toBe(8);
+  });
 
-test("default: No of Numbers: 2", () => {
-    expect(numcountDefault(passgenJs())).toBe(2)
-});
-test("default: No of Aplhabets: 4", () => {
-  expect(aplhaCountDefault(passgenJs())).toBe(4);
-});
+  test("default : No of Numbers : 2", () => {
+    expect(numcountDefault(passgenJs())).toBe(2);
+  });
+  test("default : No of Aplhabets : 4", () => {
+    expect(aplhaCountDefault(passgenJs())).toBe(4);
+  });
+
+  test("custom : Provinding String instead of Number for length : 🧛", () => {
+    expect(passgenJs("🧛")).toBe(
+      "Error : Length must be of type number"
+    );
+  });
+   test("custom : Provinding length less than 8 : 6", () => {
+     expect(passgenJs(6)).toBe("Error : Lenght must be >= 8");
+   });
+
+  test.todo("custom : Providing cutsom Length : 15")
+
+})
